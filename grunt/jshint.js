5075ea025f214a8a60bf46236d81530371185623
William Belle <william.belle@gmail.com>
2016-07-31 20:53:44 +0200

    add jshint linting with grunt

---
+++ grunt/jshint.js
@@ -0,0 +1,9 @@
+// Make sure code styles are up to par and there are no obvious mistakes
+module.exports = {
+  all: {
+    src: "**/*.js",
+    options : {
+      jshintrc : '.jshintrc'
+    }
+  }
+};
