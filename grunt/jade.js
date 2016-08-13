03a32ec595d0bab4cf192c93c5728c94e9e9a6c9
William Belle <william.belle@gmail.com>
2016-08-13 17:58:26 +0200

    put version number from package.json

--- grunt/jade.js
+++ grunt/jade.js
@@ -1,11 +1,18 @@
-module.exports = {
-  build: {
-    options: {
-      pretty: true,
+module.exports = function(grunt, options) {
+  'use strict';
+
+  return {
+    build: {
+      options: {
+        pretty: true,
+        data: function(dest, src) {
+          return grunt.file.readJSON('package.json');
+        },
+      },
+      files: {
+        'www/index.html': 'src/views/index.jade',
+        'www/status-code.html': 'src/views/status-code.jade',
+      },
     },
-    files: {
-      'www/index.html': 'src/views/index.jade',
-      'www/status-code.html': 'src/views/status-code.jade',
-    },
-  },
+  };
 };
